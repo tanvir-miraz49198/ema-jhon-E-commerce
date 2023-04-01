0539cebab14a8976877b695be32ac39aed5ada7d
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2023-04-01 11:44:12 +0600

    navbar complete

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css'
+import '../../images/Logo.svg'
+const Header = () => {
+    return (
+        <nav className='header'> 
+           <h3>Ema-Jhon</h3>
+           <div>
+           <a href="">Shop</a>
+           <a href="">Oeder</a>
+           <a href="">Manage History</a>
+           <a href="">Login</a>
+           </div>
+        </nav>
+    );
+};
+
+export default Header;
