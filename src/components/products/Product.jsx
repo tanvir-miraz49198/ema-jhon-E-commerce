02d240854b8bd3deae9efbe0c354c2c07330b2c6
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2023-04-09 08:47:22 +0600

    added products

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const {name,img,seller,quantity,price} = props.product
+    const {name,img,seller,ratings,price} = props.product
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h6>Name : {name}</h6>
+           <div className='details'>
+           <h6 className='product-name'>Name : {name}</h6>
+            <p className='price'>Price : ${price}</p>
+            <p className='manufacturer'>Manufacturer : {seller}</p>
+            <p className='ratings'>Ratings : {ratings} Star</p>
+           </div>
+           <button className='cart-btn'>Add To Cart</button>
         </div>
+        
     );
 };
 
