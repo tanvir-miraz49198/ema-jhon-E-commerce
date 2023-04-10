d57cf76c07ada7886e419a2e7c237faf8510a1bb
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2023-04-10 22:55:30 +0600

    cart 2nd update

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css'
 import Product from '../products/Product';
 import Cart from '../Cart/Cart';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 const Shop = () => {
 
     const [products, setProducts] = useState([]);
@@ -14,6 +14,33 @@ const Shop = () => {
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
+
+
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const savedCart = []
+        // step 1 : ( get id )
+        for(const id in storedCart){
+            console.log(id)
+            // step 2 : get products by using id
+            const saveProducts = products.find(products => products.id === id)
+
+            if(saveProducts){
+
+            // step 3 : get quantity of the product
+            const quantity =  storedCart[id];
+            saveProducts.quantity = quantity;
+            savedCart.push(saveProducts)
+            console.log(saveProducts)
+            }
+        }
+        setCart(savedCart)
+    }, [products])
+
+
+
+
+
     const btnClicked = (product) => {
         let newCart = [...cart, product];
         setCart(newCart)
@@ -38,7 +65,7 @@ const Shop = () => {
             <div className='cart-container'>
 
                 <Cart
-                cart = {cart}
+                    cart={cart}
                 ></Cart>
             </div>
         </div>
